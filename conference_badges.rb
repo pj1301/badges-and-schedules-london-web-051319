a8f8a77cb20e005f56b822f186c1c019a9e27d05
James Hayward <james.hayward6@icloud.com>
2019-04-22 01:33:54 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+  def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+
+  def batch_badge_creator(attendees)
+    i = 0
+    while i < attendees.length
+      badges = ["Hello, my name is #{attendees[i]}."]
+    # puts "Hello, my name is #{attendees[i]}."
+    i += 1
+  end
+ end
+
+  #
+  #   attendees.each do |name|
+  #     puts "Hello, my name is #{name}."
+  #     attendees += 1
+  #   end
+  # end
